6197270763e52dbadecb7da288e019c5e5a5e2f9
carlos <carlos_floresmontes@hotmail.com>
2021-03-29 12:50:37 -0600

    modificaciones PAGINAS index, login "OPCIONAL", LISTA DE SOLCITUD

--- SSS/View/index.php
+++ SSS/View/index.php
@@ -1,28 +1,63 @@
 <!DOCTYPE>
-
 <html>
-
-<head><title></title>
+    
 <link rel="stylesheet" href="../Source/css/index.css">
-<link rel="icon" href="">
-</head>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+<head><title>index</title></head>
+
 <body>
 
-<header>
-<div  class="logo-head"><a href="https://www.google.com"><img src="../Source/img/header_cenidet.jpg" class="img-logo"></a></div>
-</header>
-
-<div class="line-menu"></div>
-<div class="menu-div">
-<a href="registerMail.php">SOLICITUD</a>
-<a href="empleado.php">DOCENTE</a>
-<a href="listSolic.php">LISTA SOLICITUDES</a>
-<a href="#">LIBERACION</a>
+<div  class="logo-head"> 
+        <div clas="logo-item"><h2>CENTRO NACIONA DE INVESTIGACION
+        Y DESARROLO TECNOLOGICO</h2></div>
+        <div clas="logo-item"><a href="https://cenidet.tecnm.mx/">  
+        <img src="../Source/img/tecnm.png" class="img-logo"></a></div>
+        <div clas="logo-item"><h2>INSTITUTO TECNOLOGICO DE MEXICO</h2></div>
 </div>
 
-<div class="img-page-consts"><img src="../Source/img/pagina-en-construcción.jpg"></div>
+
+
+<header>
+<div class="line-menu"></div>
+    <div class="menu-div">
+    <a href="Login.php">SOLICITUD</a>
+    <a href="empleado.php">DOCENTE</a>
+    <a href="listSolic.php">LISTA SOLICITUDES</a>
+    <a href="#">LIBERACION</a>
+    </div>
+</header>
+
+<div class="body-index">
+<div class="img-page-consts">
+<img src="../Source/img/servicio.png">
+</div>
+
+
+EL L.I. ESTEBAN BUENO ES EL REPONSABLE DE ATENDER LA RESERVACION DEL AUDITORIO
+
+</div>
 </body>
-<footer><div class="content-footer">
-    
-</div></footer>
+<!-- <footer><div class="content-footer">
+    <li>REDES SOLCIALES</li>
+</div>
+<div>
+<ul class="breadcrumb">
+                  <li>ALGUNOS DERECHOS RESERVADOS © 2017<span class="divider">|</span></li>
+                  <li><a href="politicas-de-privacidad.php">POLÍTICAS DE PRIVACIDAD</a> <span class="divider">|</span></li>
+                  <li><a href="contacto.php">CONTACTO</a> <span class="divider">|</span></li>
+                  <li><a href="servicios.php">SERVICIOS</a> <span class="divider">|</span></li>
+                  <li><a href="partners.php">PARTNERS</a> <span class="divider">|</span></li>
+            </ul>
+</div>
+ <div class="Gob"><img src="../Source/img/logo_footer.png">
+ <div class="span12">  
+                  	<h2 class="h2-footer">Centro Nacional de Investigación y Desarrollo Tecnológico</h2>
+                  	<p>Interior Internado Palmira S/N, Col. Palmira, C.P. 62490</p>
+                  	<p>Cuernavaca, Morelos.</p>
+                  	<p>Tel. 01(777) 362-7770</p>
+                  	<p>www.cenidet.edu.mx</p>
+                </div></div>   
+</footer>
+-->
 </html>
