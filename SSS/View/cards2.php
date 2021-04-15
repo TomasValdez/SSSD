7fbd51c2723be11e7a8d511eb831f66477d05e55
TomasValdez <67929859+TomasValdez@users.noreply.github.com>
2021-04-15 11:37:11 -0500

    Actualizacion de Menu

--- SSS/View/cards2.php
+++ SSS/View/cards2.php
@@ -8,7 +8,7 @@ $form2="</form>";?>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="../oficial.css">
+   <link rel="stylesheet" href="../Source/css/oficial.css">
    <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Pragati+Narrow&display=swap" rel="stylesheet">    <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
 
---
+++ SSS/View/men.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu de Solicitudes</title>
+    <link rel="stylesheet" href="../Source/css/menus.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+</head>
+
+
+
+
+<body>
+
+<div  class="logo-head" > 
+        <div class="text-item1"><h2>CENTRO NACIONAL DE INVESTIGACION Y DESARROLLO TECNOLOGICO</h2></div>
+        <div ><a href="https://cenidet.tecnm.mx/">  
+        <img src="../Source/img/tecnm.png" class="img-logo"></a></div>
+        <div ><a href="https://cenidet.tecnm.mx/">  
+        <img src="../Source/img/ce_log.jpg" class="img-logo2"></a></div>
+        <div class="text-item2"><h2>INSTITUTO TECNOLOGICO NACIONAL DE MEXICO</h2></div>
+</div>
+<!--
+<header>
+<div class="line-menu"></div>
+    <div class="menu-div">
+    <a href="Login.php">SOLICITUD</a>
+    <a href="listSolic.php">LISTA DE SOLICITUDES</a>
+    <a href="#">LIBERACION</a>
+    <a href="LoginTec.php">TECNICO</a>
+  
+    </div>
+</header>
+-->
+    <div class="container" >
+       
+        <div class="card">
+            <img src="../Source/img/PC.jpg">
+            <h4>Equipo de Computo</h4>
+            <p><li>No enciende el equipo</li>
+            <li>Ruidos extraños</li></p>
+            <a href="Solicitud.php">Solicitar</a>
+        </div>
+        
+        <div class="card">
+            <img src="../Source/img/SII.png">
+            <h4>SII</h4>
+            <p><li>Problemas de inicio sesion</li>
+            <li>Servidor caido</li>
+            <li>No carga</li></p>
+            <a href="Solicitud.php">Solicitar</a>
+        </div>
+        
+        <div class="card">
+            <img src="../Source/img/INTERNET.png">
+            <h4>Internet</h4>
+            <p><li>El equipo no se conecta a internet</li>
+            <li>no funciona</li></p>
+            <a href="Solicitud.php">Solicitar</a>
+            
+        </div>
+        <div class="card">
+            <img src="../Source/img/impre.png">
+            <h4>Impresora</h4>
+            <p><li>Se atoro el papel</li>
+            <li>No enciende</li>
+            <li>No funciona</li></p>
+            <a href="Solicitud.php">Solicitar</a>
+            
+        </div>
+        <div class="card">
+            <img src="../Source/img/telefonia.jpeg">
+            <h4>Telefonia</h4>
+            <p><li>No hay linea</li>
+            <li>No funciona</li></p>
+            <a href="Solicitud.php">Solicitar</a>
+            
+        </div>
+        <div class="card">
+            <img src="../Source/img/SIGNO.png">
+            <h4>Otro</h4>
+            <p>
+            <textarea class="textarea"Style="resize:none" >Detalles</textarea></li></p>
+            <a href="Solicitud.php">Solicitar</a>
+            
+        </div>
+        
+    </div>
+    
+</body>
+</html>
