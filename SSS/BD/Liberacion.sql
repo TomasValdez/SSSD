9408cd7a70d21d3bbd5a793178caf4911f243984
TomasValdez <67929859+TomasValdez@users.noreply.github.com>
2021-04-16 11:20:34 -0500

    BD

---
+++ SSS/BD/Liberacion.sql
@@ -0,0 +1,7 @@
+<-- REGISTRO DE SOLCITUD LIBERADO-->
+DELIMITER //
+CREATE PROCEDURE liberation(in  _id int,in _valoration  int ,in _comentario varchar (250))
+BEGIN 
+UPDATE Registro SET  statusS="Liberado" , FechaL=date(now()) , valoracion=_valoration , Comentario=_comentario where idRegistro=_id;
+END //
+DELIMITER ;
---
+++ SSS/BD/REGISTER SOLCITUD.sql
@@ -0,0 +1,16 @@
+SET @ID=(SELECT max(idRegistro) from registro2 where activacion=1 and statusS="Espera" order by idregistro desc ); /*buscar idregistro que este en espera*/
+
+	SET @tipo=(SELECT tipoS   from registro2  where idRegistro=@ID limit 1)  ;
+	SET @correo=(SELECT correoSolic  from registro2  where idRegistro=@ID limit 1);
+    
+	SET @Area=(SELECT em.adscripcion from Empleado em WHERE Correo=@correo
+	union
+	SELECT es.Area  from Estudiante es WHERE Correo=@correo limit 1);
+
+	select tec.idtecnico  from tecnico tec inner join habilidad hab on tec.idtecnico=hab.idtecnico WHERE Habilidad=@tipo
+		and tec.idTecnico not in(select idTecnico from registro2 where statusS="Asignado" and tipoS=@tipo group by  idTecnico ) 
+        and   departamento = ('M-Electrónica' )
+		order by Prioridad desc limit 1;
+        
+        
+        select idTecnico from registro2 where statusS="Asignado" and tipoS="SOFTWARE" group by  idTecnico;
